7b5a7773a55d518151b58c2fbb6969f28d0045c9
Bryce <brycejensenius@gmail.com>
2024-08-07 21:48:27 -0500

    Filtering by name added

--- src/main/java/com/BryceJensenius/MediaOrganizer/controller/MediaController.java
+++ src/main/java/com/BryceJensenius/MediaOrganizer/controller/MediaController.java
@@ -1,5 +1,6 @@
 package com.BryceJensenius.MediaOrganizer.controller;
 
+import com.BryceJensenius.MediaOrganizer.model.FilterRequest;
 import com.BryceJensenius.MediaOrganizer.model.MediaItem;
 import com.BryceJensenius.MediaOrganizer.service.MediaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +15,25 @@ public class MediaController {
     @Autowired
     private MediaService mediaService;
 
+    private String filter = "";
+
     @PostMapping("/add")
     public String add(@RequestBody MediaItem media){
         mediaService.saveMedia(media);
         return "New Media Item Was Added";
     }
 
+    @PostMapping("/setFilter")
+    public void setFilter(@RequestBody FilterRequest filterRequest) {
+        this.filter = filterRequest.getFilter(); // Get the filter from the request object
+    }
+
     @GetMapping("/getAll")
-    public List<MediaItem> getAllMedia(){
-        return mediaService.getAllMedia();
+    public List<MediaItem> getAllMedia() {
+        List<MediaItem> mediaList = mediaService.getAllMedia();//get media list from database
+        System.out.println(filter);
+        //remove all media without the filter in the name
+        mediaList.removeIf(m -> !m.getName().toLowerCase().contains(filter.toLowerCase()));
+        return mediaList;
     }
 }
---
+++ src/main/java/com/BryceJensenius/MediaOrganizer/model/FilterRequest.java
@@ -0,0 +1,13 @@
+package com.BryceJensenius.MediaOrganizer.model;
+
+public class FilterRequest {
+    private String filter;
+
+    public String getFilter() {
+        return filter;
+    }
+
+    public void setFilter(String filter) {
+        this.filter = filter;
+    }
+}
