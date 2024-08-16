1b89c99b63999c0f62d0f8ec1c95c6993100f02b
Bryce <brycejensenius@gmail.com>
2024-08-15 19:41:45 -0500

    Feat: finished nameGuess List functionality, correctly returned list

--- src/main/java/com/BryceJensenius/MediaOrganizer/controller/OmdbController.java
+++ src/main/java/com/BryceJensenius/MediaOrganizer/controller/OmdbController.java
@@ -2,21 +2,24 @@ package com.BryceJensenius.MediaOrganizer.controller;
 
 import com.BryceJensenius.MediaOrganizer.service.OmdbService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.google.gson.JsonObject;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/api/omdb")
+@CrossOrigin
 public class OmdbController {
 
     @Autowired
     private OmdbService omdbService;
+    private int numCalls = 0;
 
-    @GetMapping("/movie")
-    public JsonObject getMovie(@RequestParam String title) {
-        return omdbService.getMovieDetails(title);
+    @GetMapping("/movie/{title}")
+    public String[] getMovie(@PathVariable String title) {
+        String[] guess = omdbService.getMovieDetails(title);
+        System.out.println(Arrays.toString(guess));
+        return guess;
     }
 }
--- src/main/java/com/BryceJensenius/MediaOrganizer/service/OmdbService.java
+++ src/main/java/com/BryceJensenius/MediaOrganizer/service/OmdbService.java
@@ -1,8 +1,13 @@
 package com.BryceJensenius.MediaOrganizer.service;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class OmdbService {
@@ -11,14 +16,34 @@ public class OmdbService {
     private final String OMDB_URL = "http://www.omdbapi.com/?apikey=" + API_KEY + "&";
 
     /*
-        Get a Movie's Details from it's title
+        Guess a Movie's Title from partially complete title
      */
-    public JsonObject getMovieDetails(String title) {
-        String url = OMDB_URL + "t=" + title.replace(" ", "+");
+    public String[] getMovieDetails(String title) {
+        String url = OMDB_URL + "s=" + title.replace(" ", "+");
         RestTemplate restTemplate = new RestTemplate(); //rest template requests can throw errors, add handling later
-        String jsonResponse = restTemplate.getForObject(url, String.class);
+        try{
+            String jsonResponse = restTemplate.getForObject(url, String.class);
+            JsonObject jsonObject = JsonParser.parseString(jsonResponse).getAsJsonObject();
 
-        JsonObject jsonObject = JsonParser.parseString(jsonResponse).getAsJsonObject();
-        return jsonObject;
+            //List is contained in a Search entry, and entries inside should be JSON Array
+            if(jsonObject.has("Search") && jsonObject.get("Search").isJsonArray()){
+                JsonArray jsonArray = jsonObject.getAsJsonArray("Search");//grab search and get it's list
+                List<String> movieNamesList = new ArrayList<>();//list of name guesses
+
+                for(JsonElement media : jsonArray) {//arrays have elements, the elements must be converted to json object
+                    JsonObject mediaObject = media.getAsJsonObject();
+                    if(mediaObject.has("Title")){//ensure object returned has a title before grabbing it
+                        movieNamesList.add(mediaObject.get("Title").getAsString());//convert JSON Title to string
+                    }
+                }
+                return movieNamesList.toArray(new String[0]);//input of array for elements to be stored in, 0 has size chosen internally
+            }else {
+                return new String[]{"No Results"};
+            }
+        }catch(NullPointerException e) {
+            return new String[]{"Null Error Occurred"};
+        }catch(Exception e){
+            return new String[]{"API Call Failed"};
+        }
     }
 }
