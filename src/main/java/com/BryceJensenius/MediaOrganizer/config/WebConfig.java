0cf0a9984b075c25e014f5b812790b68689b264e
Bryce <brycejensenius@gmail.com>
2025-12-20 13:11:19 -0600

    Add WebConfig to allow CrossOrigin requests

---
+++ src/main/java/com/BryceJensenius/MediaOrganizer/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.BryceJensenius.MediaOrganizer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+    Configuration class to set up CORS settings for the application
+    This allows cross-origin requests from specified origins
+    Frontend was being blocked since I started sending post requests with JSON bodies
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://www.brycejensenius.xyz")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
