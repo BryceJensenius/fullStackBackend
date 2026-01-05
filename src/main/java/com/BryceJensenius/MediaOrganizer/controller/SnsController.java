e56e9d34c9cdda24a83a3245c28f93629ea7fe50
Bryce <brycejensenius@gmail.com>
2026-01-04 18:04:20 -0600

    Remove SNS endpoint

--- src/main/java/com/BryceJensenius/MediaOrganizer/controller/SnsController.java
+++ src/main/java/com/BryceJensenius/MediaOrganizer/controller/SnsController.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.BryceJensenius.MediaOrganizer.model.SnsBody;
+import com.BryceJensenius.MediaOrganizer.model.User;
 import com.BryceJensenius.MediaOrganizer.service.SnsService;
+import com.BryceJensenius.MediaOrganizer.service.UserService;
 
 @RestController
 @RequestMapping("/publishSns")
@@ -17,8 +20,15 @@ public class SnsController {
     @Autowired
     private SnsService snsService;
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping
-    public void publishToSns(@RequestBody SnsBody snsBody){
-        snsService.publishSnsMessage(snsBody);
+    public void publishToSns(@RequestHeader("Authorization") String authHeader, @RequestBody SnsBody snsBody){
+        // User user = userService.getUserFromAuthorizationHeader(authHeader);
+        // if(user == null){
+        //     return;
+        // }
+        // snsService.publishSnsMessage(snsBody);
     }
 }
